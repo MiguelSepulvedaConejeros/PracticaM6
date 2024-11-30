5e7db12f28b3ee4bddf11e7c78a32c8bd34ce4e3
MiguelSepulvedaConejeros <msepco@gmail.com>
2024-11-30 17:08:33 -0300

    practica

---
+++ practica/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view/>
+</template>
+
+<script lang="js">
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    name: 'App',
+ }) //hola
+</script>
+
+<style></style>
---
+++ practica/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import router from './router'; // Importa el router
+
+const app = createApp(App);
+app.use(router); // Usa el router
+app.mount('#app');
